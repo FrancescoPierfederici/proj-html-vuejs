e724238ba7329ba8ff66867dd3e0e2202d1b34b5
FrancescoPierfederici <francescopierf@gmail.com>
2021-05-25 11:14:39 +0200

    commit

--- myscript.js
+++ myscript.js
@@ -1,4 +1,40 @@
 
+new Vue({
+    el: '#app',
+    data: {
+        navbarLink:
+            [{
+                text:"HOME",
+                link:"#",
 
+            },
+            {
+                text:"COURSES",
+                link:"#",
 
+            },
+            {
+                text:"INSTRUCTORS",
+                link:"#",
+
+            },
+            {
+                text:"EVENTS",
+                link:"#",
+
+            },
+            {
+                text:"PAGES",
+                link:"#",
+
+            },
+            {
+                text:"ELEMENTS",
+                link:"#",
+
+            }
+        ]  
+        
+    },
+});
 
